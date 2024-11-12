b17a2b2e6cabda5a1316bb6d5404504cc2460b61
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-12 17:19:35 -0300

    todolist.py

--- todolist.py
+++ todolist.py
@@ -1,13 +1,14 @@
 def addTarefa(lista, tarefa):
     while True:
         if(tarefa == None | tarefa.strip() == ""):
-            print("Erro: tarefa nula ou vazio.")
+            print("Erro: tarefa nula ou vazia.")
         else:
             lista.append(tarefa)
 def encontrarPorCodigo(lista,codigo):
-    list(filter(lambda x: x["codigo"] == codigo,lista))[0]
+    return list(filter(lambda x: x["codigo"] == codigo,lista))[0]
 def concluirTarefa(lista, codigo):
     tarefa = encontrarPorCodigo(lista,codigo)
-    return tarefa
-tarefas = [{"codigo": 1, "descrição": "Estudar python", "concluido": True}]
-print(concluirTarefa(tarefas,1))
+    tarefa["concluido"] = True
+tarefas = [{"codigo": 1, "descrição": "Estudar python", "concluido": False}]
+concluirTarefa(tarefas,1)
+print(tarefas)
