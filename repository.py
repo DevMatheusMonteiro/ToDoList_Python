db7f14b4a58dfb40ba2a5bf2b67df004f3d97978
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2024-11-16 10:51:00 -0300

    repository.py

--- repository.py
+++ repository.py
@@ -1,15 +1,23 @@
+""" Camada intermediária entre a aplicação e a fonte de dados """
 from mock import tarefas
 def addTarefa(tarefa):
+    """ Função para adicionar tarefa à fonte de dados """
     tarefas.append(tarefa)
 def removerTarefa(tarefa):
+    """ Função para remover tarefa da fonte de dados """
     tarefas.remove(tarefa)
-def encontrarPorCodigo(codigo):
-    return list(filter(lambda tarefa: tarefa["codigo"] == codigo,tarefas))
+def encontrarPorId(id):
+    """ Função para encontrar uma tarefa pelo id """
+    return list(filter(lambda tarefa: tarefa["id"] == id,tarefas))
 def tarefasPendentes():
+    """ Função que retornará tarefas pendentes """
     return list(filter(lambda x:x["concluido"]==False,tarefas))
 def tarefasConcluidas():
+    """ Função que retornará tarefas concluídas """
     return list(filter(lambda x:x["concluido"]==True,tarefas))
 def todasTarefas():
+    """ Função que retornará todas as tarefas"""
     return tarefas
 def concluirTarefa(tarefa):
+    """ Função que atualizará a tarefa para concluída """
     tarefa["concluido"] = True
