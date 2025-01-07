fd5a897b53d1834ff313464aed41b8c28e193847
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-01-07 19:28:52 -0300

    testes finalizados

--- src/tests.py
+++ src/tests.py
@@ -10,3 +10,40 @@ def test_create_task():
     response_json = response.json()
     assert "message" in response_json
     assert "id" in response_json
+    tasks.append(response_json["id"])
+
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    assert "tasks" in response.json()
+    assert "total_tasks" in response.json()
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        assert task_id == response.json()["id"]
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        task = {"title": "Tarefa 1", "description": "Mudando descrição pelo teste update", "completed": True}
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=task)
+        assert response.status_code == 200
+        response_json = response.json()
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        for key in task.keys():
+            assert response.json()[key] == task[key]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
+        
